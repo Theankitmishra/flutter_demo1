bb02ed71743d7806509336abdb877963dc371d60
Ankit Kumar Mishra <ankitkumarmishra@Ankits-MacBook-Air.local>
2022-11-17 21:41:00 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("welcome"),
+    );
+  }
+}
