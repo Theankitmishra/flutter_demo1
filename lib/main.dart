f0546319536643c6ac0bcbe6777a8c1ad54dc797
Theankitmishra <theankitmishraa@gmail.com>
2022-12-11 08:42:33 -0800

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo1/pages/home_page.dart';
+import 'package:flutter_demo1/core/store.dart';
+import 'package:flutter_demo1/pages/cart_page.dart';
 import 'package:flutter_demo1/pages/login_page.dart';
+import 'package:flutter_demo1/utils/routes.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'pages/home_page.dart';
+import 'widgets/themes.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(VxState(store: MyStore(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,14 +16,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      routes: {
-        "/": (context) => HomePage(),
-        "/login": (context) => LoginPage()
-      },
+    return MaterialApp.router(
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: VxInformationParser(),
+      routerDelegate: VxNavigator(
+        routes: {
+          "/": (_, __) => MaterialPage(child: LoginPage()),
+          MyRoutes.homeRoute: (_, __) => MaterialPage(child: HomePage()),
+          MyRoutes.loginRoute: (_, __) => MaterialPage(child: LoginPage()),
+          MyRoutes.cartRoute: (_, __) => MaterialPage(child: CartPage()),
+        },
+      ),
     );
   }
 }
